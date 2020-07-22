2074256e8cfa98d3017f8a2afd1b9d40d6f2d630
rysz-eriksson <ryszard.wolosiuk@gmail.com>
2020-07-22 19:32:36 +0200

    docs: untracking build folder

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import TextField from '@material-ui/core/TextField';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
